f6f2fe8a35de9a955d295d12ffd8a97620175809
phuonguyenne <22138117@st.hcmuaf.edu.vn>
2026-05-03 13:51:02 +0700

    Update config.php

--- config.php
+++ config.php
@@ -1,9 +1,9 @@
 <?php
 $conn = new mysqli(
-    $_ENV['MYSQLHOST'],
-    $_ENV['MYSQLUSER'],
-    $_ENV['MYSQLPASSWORD'],
-    $_ENV['MYSQLDATABASE']
+    getenv('MYSQLHOST'),
+    getenv('MYSQLUSER'),
+    getenv('MYSQLPASSWORD'),
+    getenv('MYSQLDATABASE')
 );
 
 if ($conn->connect_error) {
@@ -11,4 +11,6 @@ if ($conn->connect_error) {
 }
 
 $conn->set_charset("utf8mb4");
+
+echo "Kết nối database thành công!";
 ?>
