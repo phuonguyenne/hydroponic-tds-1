ec300280b522f756b35e20b35fdbe7f262b09a8d
phuonguyenne <22138117@st.hcmuaf.edu.vn>
2026-05-03 13:33:51 +0700

    Refactor database connection to use environment variables
    Updated database connection to use environment variables and set charset to utf8mb4.

--- config.php
+++ config.php
@@ -1,7 +1,14 @@
 <?php
-$conn = new mysqli("localhost", "root", "", "tds_system");
+$conn = new mysqli(
+    $_ENV['MYSQLHOST'],
+    $_ENV['MYSQLUSER'],
+    $_ENV['MYSQLPASSWORD'],
+    $_ENV['MYSQLDATABASE']
+);
 
 if ($conn->connect_error) {
     die("Kết nối lỗi: " . $conn->connect_error);
 }
+
+$conn->set_charset("utf8mb4");
 ?>
