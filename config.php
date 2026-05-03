730ce23db0631c0c942ea8d35ed2c446c0cb54d1
phuonguyenne <22138117@st.hcmuaf.edu.vn>
2026-05-03 13:51:45 +0700

    Refactor database connection script in config.php
    Added success message for web connection.

--- config.php
+++ config.php
@@ -1,10 +1,11 @@
 <?php
-$conn = new mysqli(
-    getenv('MYSQLHOST'),
-    getenv('MYSQLUSER'),
-    getenv('MYSQLPASSWORD'),
-    getenv('MYSQLDATABASE')
-);
+// Railway MySQL
+$servername = getenv('MYSQLHOST');
+$username = getenv('MYSQLUSER');
+$password = getenv('MYSQLPASSWORD');
+$dbname = getenv('MYSQLDATABASE');
+
+$conn = new mysqli($servername, $username, $password, $dbname);
 
 if ($conn->connect_error) {
     die("Kết nối lỗi: " . $conn->connect_error);
@@ -12,5 +13,6 @@ if ($conn->connect_error) {
 
 $conn->set_charset("utf8mb4");
 
-echo "Kết nối database thành công!";
+echo "Kết nối database thành công! <br><br>";
+echo "Nếu bạn thấy dòng này, web đã chạy OK!";
 ?>
